95cc177bcdcf5fa6bd58176de3995323e0f542d7
88 Pallav <er.kumarpallav06@gmail.com>
2024-04-29 04:21:18 +0530

    C2

--- Structure_of_program.cpp
+++ Structure_of_program.cpp
@@ -1,13 +1,28 @@
-/* KEYWORDS: Reserved for specific functionality cannot be redefined or overloaded.
-C++ has 90 
-Java has 50 
-C has 32 
-Python has 33 
-- More the keywords more the complexity of language but not all are frequently used.
+/* 
+*KEYWORDS: * Reserved for specific functionality cannot be redefined or overloaded.
+- C++ has 90 
+- Java has 50 
+- C has 32 
+- Python has 33 
+- More the keywords, more the complexity of language but not all are frequently used.
 
 Indentifiers are created by programmers. Eg. cin, cout, endl main(), #include
 Operators are +, *, /, << (Insertion Operator), >> (Extraction Operator), :: (Scope Resolution), "", {}, ;
 
-- All this together is te syntax of a program
+- All this together is syntax of a program
+
+*Pre-processor: * A program that processes your source code before compiler sees it.
+It first strips all comments from source code and replaces each comment with a single space.
+Then it looks for pre-proceessor fdirectives and then executes them. 
+
+*Pre-processor Directives: * Lines in the source code which start from '#'. Eg.
+#include, When it sees this it replaces it with file is it refering to and then recursively
+processes the file.
+Pre-processor are used to conditionally compile code.
+
+Note: Pre-processor doent understand C++. It simply follows the pre-processor directives
+and gets the source code ready for compiler. Compiler does understand pre-processor.
+
+
 */
 
--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 
 int main() {            // main function (Executin begins here)
     
-    int favourite_number;   // declaring the type and name of the variable 
+    int favourite_number;   
+    // declaring the type and name of the variable 
     std::cout << "Enter your favourite number between 1 and 100: ";    
     // 'cout' writes to the console where '<<' is extraction operator  
     
@@ -17,62 +18,60 @@ int main() {            // main function (Executin begins here)
     return 0;
 }
 
-/* Runnning code in terminal: When gcc isn't able to compile 
+/* 
+*Runnning code in terminal: *When gcc isn't able to compile 
 Compiling the file: g++ file_name.cpp -o file_name.exe
 Running the file: .\file_name.exe 
-*/
 
-/* Building a program: When compiling a program we are converting a source code (cpp file) 
+
+*Building a program: *When compiling a program we are converting a source code (cpp file) 
 into object (machine language) which will create .o/.obj file. As projects are compiled 
 you many of these object files 
 
 1. Clean: Clean process removes all those object files and puts the project in clean state
-          i.e. source code only no object files.This a non-issue if you have only one 
-          cpp file but if you have 1000s' of them this becomes very important as it'll
-          take significant amount of time to build a project.
+i.e. source code only no object files.This a non-issue if you have only one 
+cpp file but if you have 1000s' of them this becomes very important as it'll
+take significant amount of time to build a project.
 2. Build: It'll go through all the c++ files and build only what needs to be built.
 3. Re-built: It's combination of build and clean. It's gonna clean everything and then build again.
-*/
 
-/* Compiler Errors
+
+*Compiler Errors*
 Syntax Error: Something wrong with the structure eg. 
-            std::cout << "Error << std::endl;  // Here second interverted comma is missing 
-            return 0                           // here semi-clon is missing 
+std::cout << "Error << std::endl;  // Here second interverted comma is missing 
+return 0                           // here semi-clon is missing 
 
 Semantic Error: Something wrong with the meaning 
-            a + b;      // When it doesn't make sense to add a and b
-                           Eg. if 'a' is a char and 'b' is an int 
+a + b;      // When it doesn't make sense to add a and b
+Eg. if 'a' is a char and 'b' is an int 
 
 Note: When fixing error start with the first one. This might help subsequent errors resolve automatically.
-*/
 
-/* Compiler warning: Complier points a potention problem. Eg.
-        int miles_driven;
-            std::cout << miles_driven;
+*Compiler warning: *Complier points a potention problem. Eg.
+int miles_driven;
+std::cout << miles_driven;
 
-    warning: 'miles_driven' is used uninitialized which means no value assigned.
-              This may lead to any random thing being printed. 
+warning: 'miles_driven' is used uninitialized which means no value assigned.
+This may lead to any random thing being printed. 
     
-    Have a no warnings, clean compiles policy when you code.
-*/
+Have a no warnings, clean compiles policy when you code.
 
-/* Linker Error: Trouble in linking all object together to create an executable file.
+*Linker Error: *Trouble in linking all object together to create an executable file.
                  Usually a library or object file is missing
-*/
 
-/* Runtime Error: Error when the program is running. Eg. 
-                  - Divide by zero
-                  - File not found 
-                  - Out of memory
-    Can cause program crash
-    - Exception handling can help deal with runtime errors   
-*/
 
-/* Logic errors: Error or bugs that cause your program to run incorrectly, mistakes made by programmers. Eg.
-                 To program for a person to be 18 or older to vote 
+*Runtime Error: *Error when the program is running. Eg. 
+- Divide by zero
+- File not found 
+- Out of memory
+(Can cause program crash)
+Note: Exception handling can help deal with runtime errors.  
+
+*Logic errors: *Errors or bugs that cause your program to run incorrectly, mistakes made by programmers. Eg.
+To program for a person to be 18 or older to vote 
         
-        if (age > 18) {         // Here it should be '>=' or '>17'
-            std::cout << "Yes, you can vote!";
-        }
+if (age > 18) {         // Here it should be '>=' or '>17'
+std::cout << "Yes, you can vote!";
+}
 */
 
